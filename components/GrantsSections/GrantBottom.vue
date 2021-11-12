6ef0e8695425505dece10b80355d5fae581e28f8
hyongnim <409337645@qq.com>
2021-11-12 13:49:20 +0800

    chore: update grants ui

---
+++ components/GrantsSections/GrantBottom.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h2>Grantee Use Cases</h2>
+    <v-row>
+      <v-col v-for="(it, i) in granteeList" :key="i" cols="6" md="3">
+        <div class="bd-1 bg-black">
+          <div class="d-flex al-c pa-3">
+            <div class="bdrs-100 shrink-0 ov-h">
+              <v-img
+                width="40"
+                src="https://hosting.4everland.org/img/icon/tever.jpg"
+              ></v-img>
+            </div>
+            <div class="ml-2 lh-1">
+              <div class="fw-b">{{ it.title }}</div>
+              <div class="mt-1 fz-14">
+                {{ it.intro }}
+              </div>
+            </div>
+          </div>
+
+          <v-img
+            height="120"
+            src="https://hosting.4everland.org/img/empty/cover.jpg"
+          ></v-img>
+
+          <div class="pa-3 fz-14">
+            {{ it.desc }}
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <h2>Application Process</h2>
+    <process></process>
+
+    <h2>Are you ready?</h2>
+    <div class="label-1 fz-14">
+      <div>Individuals, teams and researchers</div>
+      <div class="mt-1">are all welcome to submit applications.</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Process from './GrantProcess.vue'
+
+export default {
+  components: {
+    Process,
+  },
+  data() {
+    return {
+      granteeList: [
+        {
+          title: '4everland',
+          desc: 'test',
+          intro: 'test',
+          img: '',
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ components/GrantsSections/GrantProcess.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="grant-process">
+    <div class="d-flex f-center con">
+      <template v-for="(it, i) in list">
+        <div :key="i" class="ta-c item">
+          <img :src="it.img" style="height: 75px" />
+          <div class="mt-4 fz-14 label-1">
+            {{ it.label }}
+          </div>
+        </div>
+        <div v-show="i < list.length - 1" :key="i" class="mt-3">
+          <v-icon size="50" color="#8B9CA6">mdi-chevron-right</v-icon>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          label: 'Add Accelerated & Hosted by 4EVERLAND',
+          img: require('@/assets/imgs/grants/process/code.png'),
+        },
+        {
+          label: 'Submit Your Developer Grant Application Here',
+          img: require('@/assets/imgs/grants/process/date.png'),
+        },
+        {
+          label: 'Project Review',
+          img: require('@/assets/imgs/grants/process/apps.png'),
+        },
+        {
+          label: 'Approved & Rewards',
+          img: require('@/assets/imgs/grants/process/reward.png'),
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.grant-process {
+  overflow: auto;
+  .con {
+    min-width: 560px;
+  }
+  .item {
+    width: 180px;
+  }
+}
+</style>
