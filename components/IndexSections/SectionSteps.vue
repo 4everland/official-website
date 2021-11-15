3777ad93df285bc94a36d804b496a1fe806a2621
indirasieben <indirasiebenenv79@gmail.com>
2021-11-15 21:05:27 +0800

    chore: website update2.0

--- components/IndexSections/SectionSteps.vue
+++ components/IndexSections/SectionSteps.vue
@@ -22,10 +22,10 @@
         <v-tabs-items v-model="tab" class="mt-16">
           <v-tab-item v-for="item in items" :key="item.img">
             <v-row>
-              <v-col class="step-box" cols="12" md="6" lg="6">
+              <v-col class="step-box" cols="12" md="7" lg="7">
                 <v-img :src="item.img"></v-img>
               </v-col>
-              <v-col class="step-box btn-box" cols="12" md="6" lg="6">
+              <v-col class="step-box btn-box" cols="12" md="5" lg="5">
                 <v-btn href="/#video" class="usage-btn mb-8" height="60" tile
                   >USAGE VIDEO</v-btn
                 >
