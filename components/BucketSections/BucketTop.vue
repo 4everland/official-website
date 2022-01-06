b92ef261d103cf065b08a5bf2efc5512ec14e2b4
indirasieben <indirasiebenenv79@gmail.com>
2022-01-06 14:27:50 +0800

    chore:add bucket page

--- components/BucketSections/BucketTop.vue
+++ components/BucketSections/BucketTop.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="banner">
-    <v-container>
+    <v-img
+      :src="require('~/assets/imgs/bucket/banner.png')"
+      width="100%"
+      contain
+    ></v-img>
+    <!-- <v-container class="con">
       <v-row>
-        <v-col
-          class="d-flex align-center justify-center pa-0"
-          cols="12"
-          md="5"
-          lg="5"
-        >
-          <div class="text-h4" style="max-width: 500px">
+        <v-col class="pa-0" cols="12" md="5" lg="5">
+          <div class="text-h2 tit-color" style="max-width: 500px">
             Upload. Pin. Manage. Retrieve.
           </div>
-          <div
-            class="text-body-2 mt-16 grey--text text--lighten-1"
-            style="max-width: 500px"
-          >
+          <div class="text-h5 mt-16 tit-color" style="max-width: 500px">
             Embrace the Open Web instead of the cloud. BUCKET makes it easy to
             upload, store, pin, and fetch files from IPFS in a secure,
             convenient, and efficient way, including UI, API, and CLI interface.
@@ -30,9 +27,8 @@
             >
           </p>
         </v-col>
-        <v-col cols="12" md="7" lg=""> </v-col>
       </v-row>
-    </v-container>
+    </v-container> -->
   </div>
 </template>
 <script>
@@ -45,9 +41,13 @@ export default {
 <style scoped lang="scss">
 .banner {
   width: 100%;
-  background-image: url('~/assets/imgs/bucket/banner.png');
-  .banner-img {
-    // position: absolute;
+  position: relative;
+  .con {
+    position: absolute;
+    top: 0;
   }
+}
+.tit-color {
+  color: #484d72;
 }
 </style>
