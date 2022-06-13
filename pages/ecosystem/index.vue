460e4429620ea856d0f9d8e09ac1402a630ab54a
indirasieben <indirasiebenenv79@gmail.com>
2022-06-13 14:02:29 +0800

    chore:ecosystem page dev

---
+++ pages/ecosystem/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="ecosystem">
+    <ens-top />
+  </div>
+</template>
+
+<script>
+import ensTop from '@/components/decentraliseSections/Top.vue'
+
+export default {
+  components: {
+    ensTop,
+  },
+  data() {
+    return {
+      title:
+        '4EVERLAND Bucket | Decentralized storage and bandwidth services | 4EVERLAND',
+      description:
+        'BUCKET enables users to upload, store, and fetch files from the IPFS and Arweave in a secure, convenient, and efficient manner.',
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description,
+        },
+      ],
+    }
+  },
+}
+</script>
+<style scoped>
+#ecosystem {
+  background-color: #fff;
+}
+</style>
