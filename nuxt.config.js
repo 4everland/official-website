c89bd375c806ddf110143d2595bea874cd88590f
indirasieben <indirasiebenenv79@gmail.com>
2022-08-04 18:07:20 +0800

    chore:update share info

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
       {
         hid: 'twitter:image',
         name: 'twitter:image',
-        content: 'www.4everland.org/cover.png',
+        content: 'https://4everland.org/cover.png',
       },
       {
         hid: 'twitter:domain',
@@ -65,7 +65,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'www.4everland.org/cover.png',
+        content: 'https://4everland.org/cover.png',
       },
     ],
     link: [
