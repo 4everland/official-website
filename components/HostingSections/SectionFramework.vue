f053a9753b3b7d6fda1c6163286e0950b5b2d18d
indirasieben <indirasiebenenv79@gmail.com>
2023-01-09 15:05:15 +0800

    chore:change  frameworks logo

--- components/HostingSections/SectionFramework.vue
+++ components/HostingSections/SectionFramework.vue
@@ -7,27 +7,18 @@
             Works for almost all websites and popular frameworks.
           </div>
         </v-col>
-        <v-col
-          cols="12"
-          md="12"
-          lg="12"
-          class="d-flex justify-space-between flex-wrap mt-16"
-        >
-          <div v-for="item in list" :key="item.title" class="text-center">
-            <v-img :src="item.img" max-width="161"> </v-img>
-          </div>
-        </v-col>
-        <!-- <v-col cols="12" md="12" lg="12">
-          <div class="text-center">
-            <v-btn
-              text
-              plain
-              class="text-h6 text-center grey--text text--lighten-2"
-              >View sites &amp;frameworks we support
-              <v-icon> mdi-chevron-right </v-icon></v-btn
-            >
-          </div>
-        </v-col> -->
+        <v-row class="mt-16">
+          <v-col
+            cols="6"
+            md="3"
+            lg="3"
+            v-for="item in list"
+            :key="item.title"
+            class="my-6"
+          >
+            <v-img contain :src="item.img" max-height="80"> </v-img>
+          </v-col>
+        </v-row>
       </v-row>
     </v-container>
   </div>
@@ -39,25 +30,28 @@ export default {
     return {
       list: [
         {
-          img: require('@/assets/imgs/hosting/docker.png'),
+          img: require('@/assets/imgs/hosting/logo/1.png'),
         },
         {
-          img: require('@/assets/imgs/hosting/gatsby.png'),
+          img: require('@/assets/imgs/hosting/logo/2.png'),
         },
         {
-          img: require('@/assets/imgs/hosting/react.png'),
+          img: require('@/assets/imgs/hosting/logo/3.png'),
         },
         {
-          img: require('@/assets/imgs/hosting/webflow.png'),
+          img: require('@/assets/imgs/hosting/logo/4.png'),
         },
         {
-          img: require('@/assets/imgs/hosting/hugo.png'),
+          img: require('@/assets/imgs/hosting/logo/5.png'),
         },
         {
-          img: require('@/assets/imgs/hosting/next.png'),
+          img: require('@/assets/imgs/hosting/logo/6.png'),
         },
         {
-          img: require('@/assets/imgs/hosting/jekyll.png'),
+          img: require('@/assets/imgs/hosting/logo/7.png'),
+        },
+        {
+          img: require('@/assets/imgs/hosting/logo/8.png'),
         },
       ],
     }
