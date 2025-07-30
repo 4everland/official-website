d3be35fdf806061b129c2f6a3466f9dd39c42714
haifun <haifuncn@gmail.com>
2025-07-30 16:05:59 +0800

    chore: modify x image

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
       {
         hid: 'twitter:image',
         name: 'twitter:image',
-        content: 'https://booster.4everland.store/depinbanner.jpeg',
+        content: 'https://booster.4everland.store/newbanner.png',
       },
       {
         hid: 'og:url',
@@ -60,7 +60,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'https://booster.4everland.store/depinbanner.jpeg',
+        content: 'https://booster.4everland.store/newbanner.png',
       },
       {
         hid: 'shortcut-icon',
